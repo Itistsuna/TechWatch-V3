3389a0f8220a1e7bf20aa34fa593856c33c52ac0
Camille-Viardot <kyureim@gmail.com>
2020-08-17 12:23:37 +0200

    commit ejs

--- api.js
+++ api.js
@@ -1,18 +1,22 @@
 const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient('mongodb://127.0.0.1:27017', { useUnifiedTopology: true });
+const client = new MongoClient('mongodb://127.0.0.1:27017', {
+    useUnifiedTopology: true
+});
 const express = require('express')
 const app = express()
 app.use(express.json());
 app.use(
-	express.urlencoded({
-		extended: true
-	})
+    express.urlencoded({
+        extended: true
+    })
 );
 
-async function bdd (){
-    try{
+async function bdd() {
+
+    try {
         await client.connect()
-    }catch(error){
+
+    } catch (error) {
         console.log(error)
     }
 }
@@ -22,29 +26,65 @@ bdd()
 // ROUTE POST STUDENTS ------------------------------------------------------------------------------------------
 
 app.post('/students', async (req, res) => {
-    try{
+
+    try {
         await client.db('TechWatch-V3').collection('Students').insertOne({
             name: req.body.name
         })
         res.json({
-            "msg" : "recue"
+            "msg": "recue"
         })
-    }catch(error){
+
+    } catch (error) {
         console.log(error)
     }
 })
 
 // ROUTE GET STUDENTS ------------------------------------------------------------------------------------------
 
-app.get('/students', async (req, res)=>{
-    try{
+app.get('/students', async (req, res) => {
+
+    try {
         const students = await client.db('TechWatch-V3').collection('Students').find().toArray()
         res.json(students)
-    }catch(error){
+
+    } catch (error) {
         console.log(error)
     }
 })
 
-app.listen(8080, function() {
-	console.log('listening on http://localhost:8080/students');
+
+// ROUTE DELETE STUDENTS ------------------------------------------------------------------------------------------
+
+app.delete('/students', async (req, res) => {
+    try {
+        client.db('TechWatch-V3').collection('Students').deleteOne({name: req.body.name})
+        res.send('Student delete')
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8080, function () {
+    console.log('listening on http://localhost:8080/students');
 });
