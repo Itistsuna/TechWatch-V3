8b9c80a909104ed63740f13ee71a0d5f78eef655
Itistsuna <arnaudg9806@gmail.com>
2020-08-18 01:36:18 +0200

    commit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
-const methodOverride = require('method-override')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
